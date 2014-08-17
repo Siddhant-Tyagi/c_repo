454d5b563aed4e3166ba10984f095e955125761d
Siddhant-Tyagi <sidhant.tyagi@gmail.com>
2014-08-18 01:12:34 +0530

    fibbonaci series using recursion

---
+++ fibbonaci.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int fibbonaci(int);
+
+int main(void)
+  {
+	  int num, result;
+	  printf("Enter the element:");
+	  scanf("%d", &num);
+	  result = fibbonaci(num-1);
+	  printf("Result: %d\n", result);
+	  return 0;
+  }
+  
+int fibbonaci(int num)
+  {
+	  if(num==1)
+	    return 1;
+	  if(num==0)
+	    return 0;
+	  return (fibbonaci(num-1)+fibbonaci(num-2));
+  }
