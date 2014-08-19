aff3f358edf24bfe115e76cd971e62383f920d6e
Siddhant-Tyagi <sidhant.tyagi@gmail.com>
2014-08-20 03:07:30 +0530

    Added LDS question

--- lds.c
+++ lds.c
@@ -1,3 +1,23 @@
+/*Longest Decreasing Subsequence
+Given an integer array A, find the length of it's longest
+decreasing subsequence (elements of LDS are sorted in
+monotonic/strict decreasing order.
+You need to fill in a function which takes as inputs
+an integer n and an integer array A, containing n integers,
+and sets the output variable to length of it's LDS.
+
+Example:
+
+Input:
+3 {1,3,2)
+
+Output:
+2
+
+Explanation:
+{3,2} is the longest decreasing subsequence of {1,3,2}.
+*/
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
