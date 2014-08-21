fe24589c5e7fa50eef182c2de8ebfbd7ac44612d
Siddhant-Tyagi <sidhant.tyagi@gmail.com>
2014-08-22 02:57:41 +0530

    insertion sort algorithm implementation in c++

---
+++ insertion_sort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+int main()
+ {
+	std::vector<int> arr;
+	int i, j, tmp;
+	char ch = 'y';
+	for(i=0;i<10;i++)
+	 {
+		std::cin >> tmp;
+		arr.push_back(tmp);
+	 }
+	 
+	for(i=0;i<arr.size();i++)
+	 {
+		tmp = arr[i];
+		j = i-1;
+		while(tmp<arr[j] && i>0)
+		 {
+			arr[j+1] = arr[j];
+			j--;
+		 }
+		arr[j+1] = tmp;
+	 }
+	std::cout << std::endl << std::endl; 
+	std::vector<int>::const_iterator iter;
+	for(iter=arr.begin();iter!=arr.end();iter++)
+	 {
+		std::cout << (*iter) << std::endl;
+	 }
+	 
+	return 0;
+ }
