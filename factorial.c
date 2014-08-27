508407a07f7966a0efada14b0774f5ff819bb6cd
Siddhant-Tyagi <sidhant.tyagi@gmail.com>
2014-08-28 02:27:27 +0530

    solving factorial through recursion

---
+++ factorial.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int factorial(int);
+
+int main(void)
+  {
+	  int num, result;
+	  scanf("%d", &num);
+	  result = factorial(num);
+	  printf("\n%d\n", result);
+	  return 0;
+  }
+
+int factorial(int n)
+  {
+	  if(n==1)
+	    return 1;
+	  return n*factorial(n-1);
+   }
